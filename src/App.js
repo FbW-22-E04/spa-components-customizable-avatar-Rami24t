eae481194c254fdc2d7725374dc6dca66e529ead
RamS24 <AlSaadi.Rami24@gmail.com>
2022-10-17 15:15:36 +0200

    add defaults to test cases

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ export default function App() {
     <div>
       <h1>Customizable avatar component!</h1>
       <Avatar src="https://picsum.photos/400/400" size="s" type="circle" />
-      <Avatar src="https://picsum.photos/400/400" size="m" type="circle" />
+      <Avatar src="https://picsum.photos/400/400" type="circle" />
       <Avatar src="https://picsum.photos/400/400" size="l" type="circle" />
       <Avatar src="https://picsum.photos/400/400" size="xl" type="circle" />
-      <Avatar src="https://picsum.photos/400/400" size="m" type="square" />
-      <Avatar src="https://picsum.photos/400/400" size="s" type="square" />
+      <Avatar src="https://picsum.photos/400/400" size="s" />
+      <Avatar src="https://picsum.photos/400/400"/>
+      <Avatar src="https://picsum.photos/400/400" size="l" />
       <Avatar src="https://picsum.photos/400/400" size="xl" type="rounded" />
       <Avatar src="https://picsum.photos/400/400" size="l" type="rounded" />
-      <Avatar src="https://picsum.photos/400/400" size="m" type="rounded" />
+      <Avatar src="https://picsum.photos/400/400" type="rounded" />
       <Avatar src="https://picsum.photos/400/400" size="s" type="rounded" />
     </div>
   );
